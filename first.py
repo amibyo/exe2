d0c3bf65ed97f61b4c789d75a493352367ab1568
amibyo <ghoulem.mohamed@gmail.com>
2022-03-01 23:03:03 +0000

    Update first.py, .gitlab-ci.yml

--- first.py
+++ first.py
@@ -1,2 +1,21 @@
+# Import os module
 import os
-os.environ['MY_DATA'] = 'Ghoulem'
+# Import sys module
+import sys
+
+while True:
+    # Take the name of the environment variable
+    key_value = "MM"
+
+    # Check the taken variable is set or not
+    try:
+        if os.environ[key_value]:
+            print("The value of", key_value, " is ", os.environ[key_value])
+    # Raise error if the variable is not set
+        else:
+            print(key_value, 'environment variable is not set.')
+            os.environ[key_value] = 'BGM'
+    except KeyError:
+        print(key_value, 'environment variable is not set.')
+        # Terminate from the script
+        sys.exit(1)
